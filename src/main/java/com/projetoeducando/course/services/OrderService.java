c1251b9aad1599bd1f575a2d0e4666cb9291fba6
Michelangelo Morais <mickeey.m.200@gmail.com>
2025-03-18 15:31:15 -0300

    Order, Instant PART 2

---
+++ src/main/java/com/projetoeducando/course/services/OrderService.java
@@ -0,0 +1,30 @@
+package com.projetoeducando.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetoeducando.course.entities.Order;
+import com.projetoeducando.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+		
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+	
+
+}
